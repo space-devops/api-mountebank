fac895d4ecff6d093e8e920eb826a0d1bbf2d99b
Damian Bruera <1979769+MailmanPod@users.noreply.github.com>
2022-12-07 14:01:35 -0300

    Adding Logger (#4)

---
+++ pkg/logger/logger.go
@@ -0,0 +1,96 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+var myLogger *zap.Logger
+var logLevel zap.AtomicLevel
+
+type LogExtraInfo struct {
+	Key   string
+	Value interface{}
+}
+
+func InitLogger() {
+	logLevel = zap.NewAtomicLevel()
+	logLevel.SetLevel(zapcore.DebugLevel)
+
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.TimeKey = "timestamp"
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	fileEncoder := zapcore.NewJSONEncoder(encoderCfg)
+	consoleEncoder := zapcore.NewConsoleEncoder(encoderCfg)
+
+	logFile, _ := os.OpenFile("log.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	writer := zapcore.AddSync(logFile)
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(fileEncoder, writer, logLevel),
+		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), logLevel),
+	)
+	myLogger = zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+}
+
+func SetLogLevel(ll string) {
+	switch ll {
+	case "debug":
+		logLevel.SetLevel(zap.DebugLevel)
+		break
+	case "info":
+		logLevel.SetLevel(zap.InfoLevel)
+		break
+	case "warn":
+		logLevel.SetLevel(zap.WarnLevel)
+		break
+	case "error":
+		logLevel.SetLevel(zap.ErrorLevel)
+		break
+	case "fatal":
+		logLevel.SetLevel(zap.FatalLevel)
+		break
+	case "panic":
+		logLevel.SetLevel(zap.PanicLevel)
+		break
+	default:
+		logLevel.SetLevel(zap.DebugLevel)
+	}
+}
+
+func LogDebug(message string, correlationId string, extraInfo ...LogExtraInfo) {
+	logEntry(zapcore.DebugLevel, message, correlationId, extraInfo...)
+}
+
+func LogInfo(message string, correlationId string, extraInfo ...LogExtraInfo) {
+	logEntry(zapcore.InfoLevel, message, correlationId, extraInfo...)
+}
+
+func LogWarn(message string, correlationId string, extraInfo ...LogExtraInfo) {
+	logEntry(zapcore.WarnLevel, message, correlationId, extraInfo...)
+}
+
+func LogError(message string, correlationId string, extraInfo ...LogExtraInfo) {
+	logEntry(zapcore.ErrorLevel, message, correlationId, extraInfo...)
+}
+
+func LogFatal(message string, correlationId string, extraInfo ...LogExtraInfo) {
+	logEntry(zapcore.FatalLevel, message, correlationId, extraInfo...)
+}
+
+func LogPanic(message string, correlationId string, extraInfo ...LogExtraInfo) {
+	logEntry(zapcore.PanicLevel, message, correlationId, extraInfo...)
+}
+
+func logEntry(lvl zapcore.Level, message string, correlationId string, extraInfo ...LogExtraInfo) {
+	defer myLogger.Sync()
+
+	for _, info := range extraInfo {
+		myLogger.Log(lvl, message,
+			zap.String("correlationId", correlationId),
+			zap.Any(info.Key, info.Value),
+		)
+	}
+}
